da5a46bb87963d10e7362b03083ca1ed228580db
Vijay Patel <vijay.patel@dawsoncollege.qc.ca>
2025-02-19 20:39:26 -0500

    Setup eslint for typescript

---
+++ eslint.config.mjs
@@ -0,0 +1,28 @@
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  tseslint.configs.recommendedTypeChecked,
+  { 
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  {
+    ignores: [
+      "node_modules/",
+      "eslint**",
+      ".gitignore",
+      ".env",
+      "package**",
+      "tsconfig.json",
+      "**.md",
+      "prisma/**",
+      "**/generated/**"
+    ]
+  }
+);
